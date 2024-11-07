4dfa5afc3b49332773d45130c6ef4b2504ba9586
Yash Verma <yashv1752@gmail.com>
2024-11-07 12:59:41 +0530

    enabling CORS for deployed frontend

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const allowedOrigins = ['https://user-mangement-frontend.onrender.com/','http://localhost:3000']
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: allowedOrigins,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
